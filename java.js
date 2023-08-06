dc38a1a7ef72ac6ae6207acb6f8cdba4d3b2d2f3
Eliza Kaur <96721763+elizakaur@users.noreply.github.com>
2023-08-06 16:16:50 +0530

    Update java.js

--- java.js
+++ java.js
@@ -2,3 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   // Your existing code...
 
   // Hide the welcome overlay after the animation is complete
+  
+// Hide the welcome overlay after the animation is complete
+  const welcomeOverlay = document.querySelector(".welcome-overlay");
+  setTimeout(function () {
+    welcomeOverlay.classList.add("hidden");
+  }, 7000); // Adjust the time (in milliseconds) to match the duration of your animation
+});
